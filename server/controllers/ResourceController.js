b708a4af3bfb8baa42b1c3f4feee46c93cbfdea8
Israel Lugo <hostelixisrael@gmail.com>
2019-02-02 12:25:34 -0400

    [MOD] add process to load and delete resource

--- server/controllers/ResourceController.js
+++ server/controllers/ResourceController.js
@@ -1,4 +1,12 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
+const multer = require("multer");
+const config = require("../config");
+
+const uploadManager = multer({ dest: config.PATH_UPLOADS });
+
+const Resource = require("../database/models").Resource;
 
 const router = express.Router();
 
@@ -6,12 +14,45 @@ router.get("/", (req, res) => {
   console.log(req);
 });
 
-router.post("/", (req, res) => {
-  console.log(req.files);
+router.post("/", uploadManager.single("file"), (req, res) => {
+  Resource.create(req.file)
+    .then(() => {
+      res
+        .status(200)
+        .type("text")
+        .send(req.file.filename);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .type("text")
+        .send("");
+    });
 });
 
 router.delete("/:id", (req, res) => {
-  console.log(req);
+  Resource.findOne({
+    where: { filename: req.params.id }
+  })
+    .then(resource => {
+      fs.unlink(resource.path, err => {
+        if (err) throw err;
+      });
+      resource.destroy();
+    })
+    .then(() => {
+      res.status(200).json({
+        status: 200,
+        message: "resource eliminado con exito"
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "",
+        error: "internal_error",
+        status: 500
+      });
+    });
 });
 
 module.exports = router;
